287eb9fed8cfcf51e35d9d8b8c2b692189eb50c6
Akshay <akshayupadhyay3@gmail.com>
2021-11-24 10:31:46 -0800

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "issue9067851ebfc9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3c5f2d62a": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
